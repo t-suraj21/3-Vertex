62b44349cb3fbcf88e2048447fbd0f0bbd8a669e
suraj21 <surajyadav2121115@gmail.com>
2026-04-18 01:01:20 +0530

    upadte in python And NLP

--- resume-nlp-service/utils/nlp_engine.py
+++ resume-nlp-service/utils/nlp_engine.py
@@ -4,7 +4,7 @@ def load_nlp():
     try:
         return spacy.load("en_core_web_sm")
     except OSError:
-        from spacy.cli import download
+        from spacy.cli import download 
         download("en_core_web_sm")
         return spacy.load("en_core_web_sm")
 
